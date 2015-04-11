882f203350f21ab439ecf53c2b98fe1fe581d3e5
agoldvarg <agoldvarg@gmail.com>
2015-04-11 15:11:40 -0400

    start writing seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 
-parsed_csv = CSVScraper.new.all
+parsed_subjects_array = []
 
-parsed_csv.collect do |subject|
-  subject.gsub!(/ë/, 'e').gsub!(/ö/, 'o').gsub!(/ğ/, 'g').gsub!(/Ö/, 'O')
+File.read("./db/data/raw_data.csv").split("\n").each do |subject|
+  first_name = subject.split(",")[1].strip
+  last_name = subject.split(",")[0].strip
+  parsed_subjects_array << "#{first_name} #{last_name}"
 end
 
+parsed_subjects_array.collect do |subject|
+  subject.gsub!(/ë/, 'e').gsub!(/ö/, 'o').gsub!(/ğ/, 'g').gsub!(/Ö/, 'O')
+end
