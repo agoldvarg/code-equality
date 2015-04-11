4dd47524fb8bbfe911a22e2373b7d61293e2ea48
agoldvarg <agoldvarg@gmail.com>
2015-04-11 16:09:34 -0400

    write seed process

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,15 @@
 class Subject < ActiveRecord::Base
   has_many  :tweets
+
+  def has_page?(url)
+    begin
+      html = open(url)
+      true
+    rescue OpenURI::HTTPError => e
+      if e.message == '404 Not Found'
+        false
+      end
+    end
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+
 parsed_subjects_array = []
 
 File.read("./db/data/raw_data.csv").split("\n").each do |subject|
@@ -17,5 +20,16 @@ parsed_subjects_array.each do |subject|
 end
 
 sanitized_subjects.each do |subject|
-  Subject.create(name: subject)
+  s = Subject.new(name: subject)
+  base_wiki_url = "https://en.wikipedia.org/wiki/#{s.name.gsub(" ", "_")}"
+  if s.has_page?(base_wiki_url)
+    data = Nokogiri::HTML(open(base_wiki_url))
+    s.bio = data.css("div#mw-content-text p:nth-child(2)").text.gsub(/\[[0-9]+\]/, '') + "  (Source: Wikipedia)"
+    s.wiki_page_link = base_wiki_url
+    s.has_wiki_page = true
+  else
+    s.has_wiki_page = false
+    s.wiki_create_link = "https://en.wikipedia.org/w/index.php?title=#{s.name.gsub(" ", "_")}&action=edit"
+  end
+  s.save
 end
