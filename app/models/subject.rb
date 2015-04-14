3c6e2ef919889ffb12183b758866da3f42b71512
agoldvarg <agoldvarg@gmail.com>
2015-04-13 20:43:45 -0400

    add tweet_pull callback to Subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,8 +2,8 @@ class Subject < ActiveRecord::Base
   has_many  :tweets
   after_create :pull_tweets
 
+
   def has_page?(url)
-    # binding.pry
     begin
       html = open(url)
       true
@@ -15,6 +15,14 @@ class Subject < ActiveRecord::Base
   end
 
   def pull_tweets
-
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["twitter_key"]
+      config.consumer_secret = ENV["twitter_secret"]
+    end
+    client.search(self.name, result_type: type = "mixed", lang: "en").take(5).each do |tweet|
+      html = client.oembed(tweet.id).html
+      Tweet.create(html: html, subject: self)
+    end
   end
+
 end
